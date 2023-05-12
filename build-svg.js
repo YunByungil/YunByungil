ecd6ae47b10767caa4fd8d7f816a48365728a8c2
YunByungil <dbsquddlfz@naver.com>
2023-05-12 15:49:42 +0900

    Update README.md

--- build-svg.js
+++ build-svg.js
@@ -35,6 +35,7 @@ weather.setLang('en')
 weather.setCoordinate(37.517235, 127.047325)
 weather.setUnits('imperial')
 weather.setAPPID(WEATHER_API_KEY)
+console.log(weather.getCoordinate());
 console.log(weather.getCityId()); // 체크하는 곳
 weather.getWeatherOneCall(function (err, data) {
     if (err) console.log(err)
